f53f4d9f3bce2b5db894cc737871a0fc3c5d4d25
ManuCruzSanchez <manuelcruzsanchez@alumno.ieselrincon.es>
2025-04-02 18:55:56 +0100

    users formulary to add

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -10,8 +10,11 @@ function Users() {
         <section>
           <div className="background">
             <div>
-              <h1>titulo</h1>
-                <p className="pagetext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit at quos commodi nemo voluptatum eius labore atque vel, iste natus laboriosam optio earum nesciunt, mollitia suscipit soluta dolores qui! Nemo?</p>
+              <h1>Jugadores locales</h1>
+                <p className="pagetext">En esta página puedes introducir tu nombre y juego insignia, y de esta forma ser visible a demás jugadores para poder encontar oponentes más facilmente. Una vez estés en el local, bastará con preguntar por ellos y, ¡A LUCHAR!</p>
+                <p>agregar formulario</p>
+                <p className="pagetext">Aquí tienes la lista de los jugadores registrados:</p>
+                <p>agregar lista de firebase "USUARIOS"</p>
             </div>
           </div>
         </section>
